727b93aafa46071ab6494e64b4f54366d290fd3c
Gabriel Alves da Silva <gsilva@softexpert.com>
2023-04-27 10:51:53 -0300

    scroll

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './Navbar.css';
 
+import { Link } from 'react-scroll';
+
 function Navbar() {
     return (
         <header>
             <h3>REACT.</h3>
             <nav>
-                <a rel="noopener"  href='#home'><span>#</span>home</a>
-                <a rel="noopener"  href='#about'><span>#</span>about</a>
-                <a rel="noopener"  href='#exercise'><span>#</span>exercises</a>
+                <Link to="/" spy={true} smooth={true} offset={10} duration={300}><span>#</span>home</Link>
+                <Link to="about" spy={true} smooth={true} offset={10} duration={300}><span>#</span>about</Link>
+                <Link to="exercise" spy={true} smooth={true} offset={10} duration={300}><span>#</span>exercises</Link>
             </nav>
         </header>
       );
